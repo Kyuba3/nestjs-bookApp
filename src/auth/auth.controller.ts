5b9db12dfc456daae61416423271dc83d4186120
Kuba Kołodziej <jmk.kolodziej@gmail.com>
2023-05-17 17:44:18 +0200

    Add logging endpoint with local-strategy and local-guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDTO } from './dtos/register.dto';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {
     this.authService = authService;
+  }
+
+  @Post('/register')
+  register(@Body() registerData: RegisterDTO) {
+    return this.authService.register(registerData);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return req.user;
   }
 }
