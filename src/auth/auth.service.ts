fe55bad54b4c450a078c51068b4cc4e26afbd826
Kuba Kołodziej <jmk.kolodziej@gmail.com>
2023-05-17 17:19:47 +0200

    Add registration method for /api/auth/register endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { RegisterDTO } from './dtos/register.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {
     this.usersService = usersService;
+  }
+
+  public async register(registrationData: RegisterDTO) {
+    const hashedPassword = await bcrypt.hash(registrationData.password, 10);
+    const userData = {
+      email: registrationData.email,
+    };
+    return this.usersService.create(userData, hashedPassword);
   }
 }
